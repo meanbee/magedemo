503d1cf236f666ba592ee41d33d8f9b00b210674
Tomas Gerulaitis <tomas.gerulaitis@meanbee.com>
2014-03-10 12:36:29 +0000

    Create MageDemo application.

---
+++ src/Application.php
@@ -0,0 +1,8 @@
+<?php
+namespace Meanbee\MageDemo;
+
+class Application extends \Symfony\Component\Console\Application {
+    public function __construct() {
+        parent::__construct();
+    }
+}
