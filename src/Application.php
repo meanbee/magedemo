98997e3f528f5e86cbcb78619ab463abb15a3027
Tomas Gerulaitis <tomas.gerulaitis@meanbee.com>
2014-03-18 11:07:16 +0000

    Add application name and version.

--- src/Application.php
+++ src/Application.php
@@ -1,8 +1,18 @@
 <?php
 namespace Meanbee\MageDemo;
 
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Output\ConsoleOutputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
 class Application extends \Symfony\Component\Console\Application {
+
+    const APP_NAME = "Mage Demo";
+    const APP_VERSION = "1.0.0";
+
     public function __construct() {
-        parent::__construct();
+        parent::__construct(self::APP_NAME, self::APP_VERSION);
     }
 }
