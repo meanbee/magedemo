1acd95bcb2db015b7ff5bf7aa93efc9872fdb5ad
Tomas Gerulaitis <tomas.gerulaitis@meanbee.com>
2014-03-19 14:33:09 +0000

    Add autoloader to Application parameters.

--- src/Application.php
+++ src/Application.php
@@ -6,7 +6,23 @@ class Application extends \Symfony\Component\Console\Application {
     const APP_NAME = "Mage Demo";
     const APP_VERSION = "1.0.0";
 
-    public function __construct() {
+    protected $autoloader;
+
+    public function __construct($autoloader = null) {
         parent::__construct(self::APP_NAME, self::APP_VERSION);
+
+        if ($autoloader !== null) {
+            $this->setAutoloader($autoloader);
+        }
+    }
+
+    public function getAutoloader() {
+        return $this->autoloader;
+    }
+
+    public function setAutoloader($autoloader) {
+        $this->autoloader = $autoloader;
+
+        return $this;
     }
 }
